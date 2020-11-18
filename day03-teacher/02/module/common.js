e598df28a8e0e7a9a0f7bf273801c834d9d6f07e
843075194 <843075194@qq.com>
2020-11-18 11:48:37 +0800

    Enter

--- day03-teacher/02/module/common.js
+++ day03-teacher/02/module/common.js
@@ -19,10 +19,16 @@ exports.getMime = function (extname) {
 exports.getFileMime = function (extname) {
 
     var data=fs.readFileSync('./data/mime.json'); //同步方法
-    
+    //readFile是读取这个mime.json里的每一项数据的
+    //所以下面mimeObj就可以拿到每一项的值
     let mimeObj=JSON.parse(data.toString());   
-   
+    //data是buffer类型的，转成json串toString()
+    // console.log(mimeObj[extname]);
     return mimeObj[extname];    
+    // let extname=path.extname(pathname);
+    //可以获取后缀名path.extname() 
+     // '.html'   '.css'  '.js'  类型这种
+    //extname是每个文件的后缀名
 
 }
 
